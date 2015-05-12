5fc906049a52fb9a32771ae9346c34ba57d31414
Alex Piggott <apiggott@ikanow.com>
2015-05-11 21:08:05 -0400

    ALEPH-19 #comment Working outline guice based DI (needs the property utils to be present to work fully)

---
+++ aleph2_management_db_service_mongodb/src/com/ikanow/aleph2/management_db/mongodb/services/MongoDbManagementDbService.java
@@ -0,0 +1,116 @@
+/*******************************************************************************
+ * Copyright 2015, The IKANOW Open Source Project.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package com.ikanow.aleph2.management_db.mongodb.services;
+
+import java.util.Optional;
+
+import com.google.inject.Inject;
+import com.ikanow.aleph2.data_model.interfaces.data_services.IManagementDbService;
+import com.ikanow.aleph2.data_model.interfaces.shared_services.ICrudService;
+import com.ikanow.aleph2.data_model.objects.data_analytics.AnalyticThreadBean;
+import com.ikanow.aleph2.data_model.objects.data_import.DataBucketBean;
+import com.ikanow.aleph2.data_model.objects.data_import.DataBucketStatusBean;
+import com.ikanow.aleph2.data_model.objects.shared.SharedLibraryBean;
+import com.ikanow.aleph2.shared.crud.mongodb.services.IMongoDbCrudServiceFactory;
+
+/** Implementation of the management DB service using MongoDB (or mock MongoDB) 
+ * @author acp
+ *
+ */
+public class MongoDbManagementDbService implements IManagementDbService {
+
+	protected IMongoDbCrudServiceFactory _crud_factory;
+	
+	/** Guice generated constructor
+	 * @param crud_factory
+	 */
+	@Inject
+	public MongoDbManagementDbService(IMongoDbCrudServiceFactory crud_factory) {
+		_crud_factory = crud_factory;
+
+		//DEBUG
+		//System.out.println("Hello world from: " + this.getClass() + ": underlying=" + crud_factory);
+	}
+	
+	/* (non-Javadoc)
+	 * @see com.ikanow.aleph2.data_model.interfaces.data_services.IManagementDbService#getSharedLibraryStore()
+	 */
+	public ICrudService<SharedLibraryBean> getSharedLibraryStore() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.ikanow.aleph2.data_model.interfaces.data_services.IManagementDbService#getPerLibraryState(java.lang.Class, com.ikanow.aleph2.data_model.objects.shared.SharedLibraryBean, java.util.Optional)
+	 */
+	public <T> ICrudService<T> getPerLibraryState(Class<T> clazz,
+			SharedLibraryBean library, Optional<String> sub_collection) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.ikanow.aleph2.data_model.interfaces.data_services.IManagementDbService#getDataBucketStore()
+	 */
+	public ICrudService<DataBucketBean> getDataBucketStore() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.ikanow.aleph2.data_model.interfaces.data_services.IManagementDbService#getDataBucketStatusStore()
+	 */
+	public ICrudService<DataBucketStatusBean> getDataBucketStatusStore() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.ikanow.aleph2.data_model.interfaces.data_services.IManagementDbService#getPerBucketState(java.lang.Class, com.ikanow.aleph2.data_model.objects.data_import.DataBucketBean, java.util.Optional)
+	 */
+	public <T> ICrudService<T> getPerBucketState(Class<T> clazz,
+			DataBucketBean bucket, Optional<String> sub_collection) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.ikanow.aleph2.data_model.interfaces.data_services.IManagementDbService#getAnalyticThreadStore()
+	 */
+	public ICrudService<AnalyticThreadBean> getAnalyticThreadStore() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.ikanow.aleph2.data_model.interfaces.data_services.IManagementDbService#getPerAnalyticThreadState(java.lang.Class, com.ikanow.aleph2.data_model.objects.data_analytics.AnalyticThreadBean, java.util.Optional)
+	 */
+	public <T> ICrudService<T> getPerAnalyticThreadState(Class<T> clazz,
+			AnalyticThreadBean analytic_thread, Optional<String> sub_collection) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.ikanow.aleph2.data_model.interfaces.data_services.IManagementDbService#getUnderlyingPlatformDriver(java.lang.Class, java.util.Optional)
+	 */
+	public <T> T getUnderlyingPlatformDriver(Class<T> driver_class,
+			Optional<String> driver_options) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
