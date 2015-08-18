49837cc7231f16b882e68253118517ff0116cefe
jfreydank <jfreydank@ikanow.com>
2015-08-18 17:19:11 -0500

    Import changes.

--- aleph2_security_service_ikanow_v1/src/com/ikanow/aleph2/security/module/IkanowV1SecurityModule.java
+++ aleph2_security_service_ikanow_v1/src/com/ikanow/aleph2/security/module/IkanowV1SecurityModule.java
@@ -1,6 +1,5 @@
 package com.ikanow.aleph2.security.module;
 
-import com.google.inject.Inject;
 import com.ikanow.aleph2.data_model.interfaces.shared_services.IServiceContext;
 import com.ikanow.aleph2.security.service.IkanowV1Realm;
 
