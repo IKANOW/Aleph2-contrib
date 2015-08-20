2d826ffe289dd82c0a5bb9a775385b82d4c1c500
jfreydank <jfreydank@ikanow.com>
2015-08-20 13:48:28 -0500

    Added V1 Credential matching.

--- aleph2_security_service_ikanow_v1/src/com/ikanow/aleph2/security/module/IkanowV1SecurityModule.java
+++ aleph2_security_service_ikanow_v1/src/com/ikanow/aleph2/security/module/IkanowV1SecurityModule.java
@@ -1,6 +1,9 @@
 package com.ikanow.aleph2.security.module;
 
+import org.apache.shiro.authc.credential.CredentialsMatcher;
+
 import com.ikanow.aleph2.data_model.interfaces.shared_services.IServiceContext;
+import com.ikanow.aleph2.security.service.IkanowV1CredentialsMatcher;
 import com.ikanow.aleph2.security.service.IkanowV1Realm;
 
 public class IkanowV1SecurityModule extends CoreSecurityModule{
@@ -14,7 +17,9 @@ public class IkanowV1SecurityModule extends CoreSecurityModule{
 		super.bindRealms();
 		
 		try {
-			bindRealm().toConstructor(IkanowV1Realm.class.getConstructor(IServiceContext.class));
+			bind(CredentialsMatcher.class).to(IkanowV1CredentialsMatcher.class);
+			bindRealm().toConstructor(IkanowV1Realm.class.getConstructor(IServiceContext.class, CredentialsMatcher.class));
+			
 		} catch (NoSuchMethodException | SecurityException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
