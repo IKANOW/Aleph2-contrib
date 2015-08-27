2bf8e39f004de9f520c2ee14f68f96b26759bd45
Alex-Ikanow <apiggott@ikanow.com>
2015-08-27 10:45:05 -0400

    ALEPH-12 More hadoop plumbing

--- aleph2_analytic_services_hadoop/src/main/java/com/ikanow/aleph2/analytics/hadoop/data_model/BasicHadoopConfigBean.java
+++ aleph2_analytic_services_hadoop/src/main/java/com/ikanow/aleph2/analytics/hadoop/data_model/BasicHadoopConfigBean.java
@@ -25,38 +25,114 @@ import java.util.Map;
 public class BasicHadoopConfigBean {
 	
 	// Some Hadoop configuration fields	
-	public final static String CONTEXT_SIGNATURE = "aleph2.px.context_signature";
-	public final static String CONFIG_STRING = "aleph2.px.config_string";
-	public final static String CONFIG_STRIGN = "aleph2.px.config_json";
+	public final static String CONTEXT_SIGNATURE = "aleph2.job.context_signature";
+	public final static String CONFIG_STRING_MAPPER = "aleph2.mapper.config_string";
+	public final static String CONFIG_JSON_MAPPER = "aleph2.mapper.config_json";
+	public final static String CONFIG_STRING_COMBINER = "aleph2.combiner.config_string";
+	public final static String CONFIG_JSON_COMBINER = "aleph2.combiner.config_json";
+	public final static String CONFIG_STRING_REDUCER = "aleph2.reducer.config_string";
+	public final static String CONFIG_JSON_REDUCER = "aleph2.reducer.config_json";
 	
 	/** An individual step in the processing chain
 	 * @author alex
 	 */
 	public class Step {
+		/** Whether this step is enabled (defaults to true) - note disabling a step might cause the pipeline to fail
+		 *  if the types then don't match)
+		 * @return
+		 */
+		public Boolean enabled() { return enabled; }
+		
+		/** The class of the mapper/combiner/reducer
+		 * @return
+		 */
 		public String entry_point() { return entry_point; }
 		
+		/** A string that is available to the mapper/combiner/reducer from the Configuration (get(CONFIG_STRING))
+		 * @return
+		 */
 		public String config_string() { return config_string; }
+		/** A JSON object that is available to the mapper/combiner/reducer in String form from the Configuration (get(CONFIG_JSON))
+		 * @return
+		 */
 		public Map<String, Object> config_json() { return config_json == null ? config_json : Collections.unmodifiableMap(config_json); }
 
+		/** Ignored except for reducers, sets a target number of reducers to use (Default: 1)
+		 * @return
+		 */
 		public Long num_tasks() { return num_tasks; }
 
-		public String output_key() { return output_key; }
-		public String output_value() { return output_value; }
+		/** Optional, overrides the class of the output key type (defaults to String)
+		 *  (Obviously needs to match both the output class implementation specified by entry point, and the input
+		 *   class implementation specified by the next stage)
+		 *   Other notes:
+		 *   - It is recommended to use fully qualified class names, though they will be inferred where possible
+		 *   - Some attempt will be made to convert between types (eg Text <-> JsonNodeWritable <-> MapWritable)
+		 * @return
+		 */
+		public String output_key_type() { return output_key_type; }
 		
+		/** Optional, overrides the class of the output value type (defaults to String)
+		 *  (Obviously needs to match both the output class implementation specified by entry point, and the input
+		 *   class implementation specified by the next stage)
+		 *   Other notes:
+		 *   - It is recommended to use fully qualified class names, though they will be inferred where possible
+		 *   - Some attempt will be made to convert between types (eg Text <-> JsonNodeWritable <-> MapWritable)
+		 * @return
+		 */
+		public String output_value_type() { return output_value_type; }
+
+		private Boolean enabled;
 		private String config_string;
 		private Map<String, Object> config_json;
 		private String entry_point;		
 		private Long num_tasks;
-		private String output_key;
-		private String output_value;
+		private String output_key_type;
+		private String output_value_type;
 	}
+	/** A list of mappers to run in a pipeline. Must be non-empty
+	 * @return
+	 */
 	public List<Step> mappers() { return mappers; }
+		
+	/** The combiner to apply in between the last mapper and then the reducer
+	 *  Optional - if left blank then no combiner is generated, if an (empty object ({}), ie Step.entry_point is null
+	 *  then the reducer is used as the combiner
+	 * @return
+	 */
 	public Step combiner() { return combiner; }
+	
+	/** The reducer to run. If not specified/disabled then no reduction stage are run (the combiner must also
+	 *  be not-present/disabled in this case)
+	 * @return
+	 */
 	public Step reducer() { return reducer; }
+	
+	/** Optional, Mappers to run after the reducer - if the reducer is not run then these finalizers are appended
+	 *  to the original mappers
+	 * @return
+	 */
 	public List<Step> finalizers() { return finalizers; }
+	
+	/** For cases where it is known that an input format is available for the given input key/value types, these can be set
+	 *  Otherwise will generate an error when the job is run (some attempt will be made to convert between formats)
+	 *  For example, where it is known that the input is an Elasticsearch query then XXX can be used
+	 *  (If the key type is overriden then the value must also be and vice versa)
+	 * @return 
+	 */
+	public String input_key_type_override() { return input_key_type_override; }
+	/** For cases where it is known that an input format is available for the given input key/value types, these can be set
+	 *  Otherwise will generate an error when the job is run (some attempt will be made to convert between formats)
+	 *  For example, where it is known that the input is an Elasticsearch query then XXX can be used
+	 *  (If the key type is overriden then the value must also be and vice versa)
+	 * @return
+	 */
+	public String input_value_type_override() { return input_value_type_override; }
 	
 	private List<Step> mappers;
 	private Step combiner;
 	private Step reducer;
 	private List<Step> finalizers;	
+	private String input_key_type_override;
+	private String input_value_type_override;
 }
--- aleph2_analytic_services_hadoop/src/main/java/com/ikanow/aleph2/analytics/hadoop/services/HadoopControllerService.java
+++ aleph2_analytic_services_hadoop/src/main/java/com/ikanow/aleph2/analytics/hadoop/services/HadoopControllerService.java
@@ -16,16 +16,31 @@
 package com.ikanow.aleph2.analytics.hadoop.services;
 
 import java.io.File;
+import java.util.Collection;
+import java.util.Optional;
+import java.util.Set;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.chain.ChainMapper;
 
 import scala.Tuple2;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ikanow.aleph2.analytics.hadoop.data_model.BasicHadoopConfigBean;
 import com.ikanow.aleph2.data_model.interfaces.data_analytics.IAnalyticsContext;
+import com.ikanow.aleph2.data_model.interfaces.shared_services.IUnderlyingService;
+import com.ikanow.aleph2.data_model.objects.data_analytics.AnalyticThreadJobBean;
+import com.ikanow.aleph2.data_model.objects.data_import.DataBucketBean;
 import com.ikanow.aleph2.data_model.objects.shared.BasicMessageBean;
 import com.ikanow.aleph2.data_model.objects.shared.GlobalPropertiesBean;
+import com.ikanow.aleph2.data_model.utils.BeanTemplateUtils;
+import com.ikanow.aleph2.data_model.utils.ErrorUtils;
+import com.ikanow.aleph2.data_model.utils.Lambdas;
+import com.ikanow.aleph2.data_model.utils.Optionals;
+import com.ikanow.aleph2.data_model.utils.SetOnce;
 
 /** Starts and stops hadoop jobs
  * @author alex
@@ -33,17 +48,98 @@ import com.ikanow.aleph2.data_model.objects.shared.GlobalPropertiesBean;
 public class HadoopControllerService {
 
 	/** Launches the specified Hadoop job
-	 * @param job_name
-	 * @param config
-	 * @return
+	 * @param job_name - the job name (in practice derived from the bucketigur
+	 * @param bucket - bucket, for context
+	 * @param jobs - the entire list of jobs for this technology, for context
+	 * @param job - the actual job to launch
+	 * @param config - the hadoop config corresponding to the job's processing params
+	 * @param context
+	 * @param local_mode_override - if true then runs locally
+	 * @return a message indicating success/failure
 	 */
-	public BasicMessageBean startJob(final String job_name, final BasicHadoopConfigBean config, final IAnalyticsContext context, boolean local_mode_override)
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public BasicMessageBean startJob(
+			final String job_name, 
+			final DataBucketBean bucket, 
+			final Collection<AnalyticThreadJobBean> jobs, final AnalyticThreadJobBean job,
+			final BasicHadoopConfigBean config, 
+			final IAnalyticsContext context, 
+			boolean local_mode_override)
 	{
-		//TODO: setup InputFormat/OutputFormat classes
+		try {
+			final ObjectMapper json_mapper = BeanTemplateUtils.configureMapper(Optional.empty());
+			final GlobalPropertiesBean globals = context.getServiceContext().getGlobalProperties();
+			
+			final  Optional<Set<Tuple2<Class<? extends IUnderlyingService>, Optional<String>>>> services = Optional.empty();
+			//TODO: need to get additional services depending on inputs
+			final String context_signature = context.getAnalyticsContextSignature(Optional.of(bucket), services);
+			final Configuration base_config = getConfig(globals, context_signature);
+	
+			//TODO: setup InputFormat/OutputFormat classes, using job
 		
-		
-		
-		return null;
+			Job task = Job.getInstance(base_config, job_name);
+			
+			//TODO: need to keep track of input classes 
+			
+			//TODO: also want to chain config so you get the last object's config
+			//(or should we have an explicit param for that?)
+			
+			final SetOnce<Void> mappers_present = new SetOnce<>();
+			Optionals.ofNullable(config.mappers()).stream()
+				.filter(mapper -> Optional.ofNullable(mapper.enabled()).orElse(true))
+				.forEach(mapper -> {
+					mappers_present.set(null);
+					final Configuration mapper_config = getConfig(globals, context_signature);
+					if (null != mapper.config_json()) {
+						mapper_config.set(BasicHadoopConfigBean.CONFIG_JSON_MAPPER, json_mapper.convertValue(mapper.config_json(), String.class));
+					}
+					if (null != mapper.config_string()) {
+						mapper_config.set(BasicHadoopConfigBean.CONFIG_STRING_MAPPER, mapper.config_string());
+					}
+					//<KEYIN, VALUEIN, KEYOUT, VALUEOUT>
+					//TODO: what should the input class type be (Object.class currently)...
+					// ... if we want to handle video then might need to be tuple2 of JsonNode and stream
+					//TODO: 
+//					ChainMapper.addMapper(task, Class.forName(mapper.entry_point()), 
+//							String.class, Object.class, outputKeyClass, outputValueClass, mapper_config);
+				});
+			
+			if (!mappers_present.isSet()) {
+				return ErrorUtils.buildErrorMessage(this, "startJob", "Found no mappers bucket={0} job={1} config={2}",
+						bucket.full_name(),
+						BeanTemplateUtils.toJson(job).toString(),
+						BeanTemplateUtils.toJson(config)
+						);				
+			}
+			
+			//(Do combiner last of all since will often just use the reducer as the combiner)
+			
+			final SetOnce<Void> combiner_present = new SetOnce<>();
+			Optional.ofNullable(config.combiner())
+				.filter(combiner -> Optional.ofNullable(combiner.enabled()).orElse(true))
+				.ifPresent(Lambdas.wrap_consumer_u(combiner -> {
+					combiner_present.set(null);
+					//TODO: handle the "use as reducer" case based on 
+					if (null != combiner.config_json()) {
+						base_config.set(BasicHadoopConfigBean.CONFIG_JSON_COMBINER, json_mapper.convertValue(combiner.config_json(), String.class));
+					}
+					if (null != combiner.config_string()) {
+						base_config.set(BasicHadoopConfigBean.CONFIG_STRING_COMBINER, combiner.config_string());
+					}
+					
+					task.setCombinerClass((Class<? extends Reducer>)(Class<?>)Class.forName(combiner.entry_point()));
+				}));
+
+			
+			//TODO: combiner (hmm can't have a different config for the combiner .. will have to use the param for that)
+			
+			//TODO: reducer (hmm can't have a different config for the reducer .. will have to use the param for that)
+			
+			return null;
+		}
+		catch (Throwable t) {
+			return ErrorUtils.buildErrorMessage(this, "startJob", ErrorUtils.getLongForm("{0}", t));
+		}
 	}
 	
 	public BasicMessageBean stopJob(final String job_name) {
@@ -55,6 +151,10 @@ public class HadoopControllerService {
 	}
 
 	//////////////////////////////////////////////////////////////////
+
+	// TODO have a validate in here
+	
+	//////////////////////////////////////////////////////////////////
 	
 	// UTILITIES
 	
@@ -62,8 +162,7 @@ public class HadoopControllerService {
 	 * @param context
 	 * @return
 	 */
-	protected Configuration getConfig(final IAnalyticsContext context){
-		final GlobalPropertiesBean globals = context.getServiceContext().getGlobalProperties();
+	protected Configuration getConfig(final GlobalPropertiesBean globals, final String context_signature){
 		final Configuration configuration = new Configuration(false);
 		
 		if (new File(globals.local_yarn_config_dir()).exists()) {
@@ -79,6 +178,10 @@ public class HadoopControllerService {
 		// Some other config defaults:
 		// (not sure if these are actually applied, or derived from the defaults - for some reason they don't appear in CDH's client config)
 		configuration.set("mapred.reduce.tasks.speculative.execution", "false");
+		
+		// The most basic Aleph2 setting:
+		configuration.set(BasicHadoopConfigBean.CONTEXT_SIGNATURE, context_signature);
+		
 		return configuration;
 	}
 	
