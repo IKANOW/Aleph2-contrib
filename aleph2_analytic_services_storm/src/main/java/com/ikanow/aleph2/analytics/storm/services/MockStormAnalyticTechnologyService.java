a22eb17dc76cd7900771452994f96c49a342967a
Alex-Ikanow <apiggott@ikanow.com>
2015-09-21 18:35:52 -0400

    ALEPH-12 Big refactor of data import manager, relegating storm to an optional contrib component

--- aleph2_analytic_services_storm/src/main/java/com/ikanow/aleph2/analytics/storm/services/MockStormAnalyticTechnologyService.java
+++ aleph2_analytic_services_storm/src/main/java/com/ikanow/aleph2/analytics/storm/services/MockStormAnalyticTechnologyService.java
@@ -15,22 +15,42 @@
  *******************************************************************************/
 package com.ikanow.aleph2.analytics.storm.services;
 
+import java.io.File;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 import com.google.inject.Inject;
 import com.google.inject.Module;
 import com.ikanow.aleph2.analytics.storm.data_model.IStormController;
 import com.ikanow.aleph2.analytics.storm.modules.MockStormAnalyticTechnologyModule;
+import com.ikanow.aleph2.data_model.interfaces.data_analytics.IAnalyticsContext;
+import com.ikanow.aleph2.data_model.objects.data_analytics.AnalyticThreadJobBean;
+import com.ikanow.aleph2.data_model.objects.data_import.DataBucketBean;
+import com.ikanow.aleph2.data_model.utils.ModuleUtils;
 
 public class MockStormAnalyticTechnologyService extends StormAnalyticTechnologyService {
 
+	public static final String DISABLE_FILE = "mock_storm_disable";
+	
 	/** Guice constructor
 	 */
 	@Inject
 	public MockStormAnalyticTechnologyService(final IStormController storm_controller) {
 		super(storm_controller);
 	}		
+	
+	@Override
+	public boolean canRunOnThisNode(
+					final DataBucketBean analytic_bucket,
+					final Collection<AnalyticThreadJobBean> jobs, 
+					final IAnalyticsContext context)
+	{
+		// For testing, if file "disable_storm" is present in yarn config then return false:
+		
+		return !(new File(ModuleUtils.getGlobalProperties().local_yarn_config_dir() + File.separator + DISABLE_FILE).exists());
+	}
+	
 	
 	/** This service needs to load some additional classes via Guice. Here's the module that defines the bindings
 	 * @return
