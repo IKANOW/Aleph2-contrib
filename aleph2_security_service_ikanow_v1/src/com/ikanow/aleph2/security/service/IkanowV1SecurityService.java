980a2c377d73c4526b7804301a36c748d0e331c6
jfreydank <jfreydank@ikanow.com>
2015-09-28 16:52:01 -0500

    Refactored secuirty service: Added loginAsSystem, added SecurityService as parent, moved methods up.

--- aleph2_security_service_ikanow_v1/src/com/ikanow/aleph2/security/service/IkanowV1SecurityService.java
+++ aleph2_security_service_ikanow_v1/src/com/ikanow/aleph2/security/service/IkanowV1SecurityService.java
@@ -16,59 +16,34 @@
 package com.ikanow.aleph2.security.service;
 
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authc.AuthenticationToken;
-import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.cache.CacheManager;
-import org.apache.shiro.mgt.DefaultSecurityManager;
 import org.apache.shiro.mgt.SecurityManager;
-import org.apache.shiro.realm.Realm;
-import org.apache.shiro.session.Session;
-import org.apache.shiro.session.mgt.DefaultSessionManager;
-import org.apache.shiro.session.mgt.SessionManager;
-import org.apache.shiro.subject.PrincipalCollection;
-import org.apache.shiro.subject.SimplePrincipalCollection;
 import org.apache.shiro.subject.Subject;
-import org.apache.shiro.util.ThreadContext;
 
 import com.google.inject.Inject;
 import com.google.inject.Module;
 import com.ikanow.aleph2.data_model.interfaces.shared_services.IExtraDependencyLoader;
-import com.ikanow.aleph2.data_model.interfaces.shared_services.IManagementCrudService;
 import com.ikanow.aleph2.data_model.interfaces.shared_services.ISecurityService;
 import com.ikanow.aleph2.data_model.interfaces.shared_services.IServiceContext;
 import com.ikanow.aleph2.data_model.interfaces.shared_services.ISubject;
-import com.ikanow.aleph2.data_model.objects.shared.AuthorizationBean;
 import com.ikanow.aleph2.security.module.IkanowV1SecurityModule;
 
-public class IkanowV1SecurityService implements ISecurityService, IExtraDependencyLoader{
+public class IkanowV1SecurityService extends SecurityService implements ISecurityService, IExtraDependencyLoader{
 	
 	
 	protected ISubject currentSubject = null;
 	private static final Logger logger = LogManager.getLogger(IkanowV1SecurityService.class);
 	@Inject
 	protected IServiceContext serviceContext;
-	protected CacheManager cacheManager;
-	Collection<Realm> realms = new HashSet<Realm>();
 	
 	@Inject
 	public IkanowV1SecurityService(IServiceContext serviceContext, SecurityManager securityManager, CacheManager cacheManager) {
-		this.serviceContext = serviceContext;
-		SecurityUtils.setSecurityManager(securityManager);
-		this.cacheManager = cacheManager;
-		if(securityManager instanceof DefaultSecurityManager){
-			SessionManager sessionManager = ((DefaultSecurityManager)securityManager).getSessionManager();
-			this.realms = ((DefaultSecurityManager)securityManager).getRealms();
-			logger.debug("Session manager:"+sessionManager);	
-		}		
+		super(serviceContext,securityManager,cacheManager);
 
 	}
 
@@ -88,81 +63,14 @@ public class IkanowV1SecurityService implements ISecurityService, IExtraDependen
 
 	}
 	
-	// Clean way to get the subject
-	private Subject getShiroSubject()
-	{
-	    Subject currentUser = ThreadContext.getSubject();// SecurityUtils.getSubject();
-
-	    if (currentUser == null)
-	    {
-	        currentUser = SecurityUtils.getSubject();
-	    }
-
-	    return currentUser;
-	}
-	
-	@Override
-	public Collection<Object> getUnderlyingArtefacts() {
-		return Collections.emptyList();
-	}
 
 	@Override
 	public <T> Optional<T> getUnderlyingPlatformDriver(Class<T> driver_class, Optional<String> driver_options) {
 		return Optional.empty();
 	}
 
-	// ***************** new functions ***********************//
-
-	public ISubject getSubject() {
-		if(currentSubject == null){
-			initUnauthorized();
-		}
-		return currentSubject;
-	}
-
-	@Override
-	public ISubject login(String principalName, Object credentials) {
-		
-		String password = (String)credentials;
-        UsernamePasswordToken token = new UsernamePasswordToken(principalName,password);
-        //token.setRememberMe(true);
-
-        ensureUserIsLoggedOut();
-        Subject shiroSubject = getShiroSubject();
-        shiroSubject.login((AuthenticationToken)token);
-        currentSubject = new SubjectWrapper(shiroSubject);
-		return currentSubject;
-	}
-
-	private void ensureUserIsLoggedOut()
-	{
-	    try
-	    {
-	    	Subject shiroSubject = getShiroSubject();
-	        if (shiroSubject == null)
-	            return;
-
-	        // Log the user out and kill their session if possible.
-	        shiroSubject.logout();
-	        Session session = shiroSubject.getSession(false);
-	        if (session == null)
-	            return;
-
-	        session.stop();
-	    }
-	    catch (Exception e)
-	    {
-	        // Ignore all errors, as we're trying to silently 
-	        // log the user out.
-	    }
-	}
-	@Override
-	public boolean hasRole(ISubject subject, String roleIdentifier) {
-		boolean ret = ((Subject)getSubject().getSubject()).hasRole(roleIdentifier);
-		return ret;
-	}
-
 	
+
 	static IkanowV1SecurityModule _temp;
 	
 	public static List<Module> getExtraDependencyModules() {
@@ -170,7 +78,10 @@ public class IkanowV1SecurityService implements ISecurityService, IExtraDependen
 	}
 
 	public void killMe() throws Exception {
-		if (null != _temp) _temp.destroy();
+		if (null != _temp) {
+			_temp.destroy();
+			
+		}
 	}
 	
 
@@ -181,65 +92,17 @@ public class IkanowV1SecurityService implements ISecurityService, IExtraDependen
 	}
 
 
-	@Override
-	public boolean isPermitted(ISubject subject, String permission) {
-		boolean ret = ((Subject)getSubject().getSubject()).isPermitted(permission);
-		return ret;
-	}
+
 
 
 	@Override
-	public void runAs(ISubject subject,Collection<String> principals) {
-		// TODO Auto-generated method stub
-		
-		((Subject)subject.getSubject()).runAs(new SimplePrincipalCollection(principals,IkanowV1Realm.class.getName()));
+	public ISubject loginAsSystem() {
+		return super.loginAsSystem();
 	}
-
-
-	@SuppressWarnings("unchecked")
-	@Override
-	public Collection<String> releaseRunAs(ISubject subject) {
-		PrincipalCollection p = ((Subject)subject.getSubject()).releaseRunAs();	
-		return p.asList();
-	}
-
 
 	@Override
-	public <O> IManagementCrudService<O> secured(IManagementCrudService<O> crud, AuthorizationBean authorizationBean) {
-		return new SecuredCrudManagementDbService<O>(serviceContext, crud, authorizationBean);
+	protected String getRealmName(){
+		return IkanowV1Realm.class.getName();
 	}
 
-	public void setSessionTimeout(long globalSessionTimeout){
-		SecurityManager securityManager = SecurityUtils.getSecurityManager();
-		if(securityManager instanceof DefaultSecurityManager){
-			SessionManager sessionManager = ((DefaultSecurityManager)securityManager).getSessionManager();
-			if(sessionManager instanceof DefaultSessionManager){
-				((DefaultSessionManager)sessionManager).setGlobalSessionTimeout(globalSessionTimeout);
-			}
-			logger.debug("Session manager:"+sessionManager);	
-		}
-	}
-
-	public void invalidateAuthenticationCache(Collection<String> principalNames){
-		for (Realm realm : realms) {
-			if(realm instanceof IkanowV1Realm){
-				IkanowV1Realm ar = (IkanowV1Realm)realm;
-				ar.clearCachedAuthorizationInfo(principalNames);
-		} 
-			
-		}
-	}
-
-	/**
-	 * This function invalidates the whole cache
-	 */
-	public void invalidateCache(){
-		for (Realm realm : realms) {
-			if(realm instanceof IkanowV1Realm){
-				IkanowV1Realm ar = (IkanowV1Realm)realm;
-				ar.clearAllCaches();
-		} 
-			
-		}
-	}
 }
