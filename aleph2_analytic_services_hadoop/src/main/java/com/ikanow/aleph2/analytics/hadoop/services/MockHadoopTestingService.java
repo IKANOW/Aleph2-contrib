6da161243c6888cc46308ca57c3a4ae4d557d15b
Alex-Ikanow <apiggott@ikanow.com>
2015-10-06 09:27:14 -0400

    ALEPH-12 Improvement to hadoop test harness

--- aleph2_analytic_services_hadoop/src/main/java/com/ikanow/aleph2/analytics/hadoop/services/MockHadoopTestingService.java
+++ aleph2_analytic_services_hadoop/src/main/java/com/ikanow/aleph2/analytics/hadoop/services/MockHadoopTestingService.java
@@ -18,7 +18,9 @@ package com.ikanow.aleph2.analytics.hadoop.services;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
@@ -90,7 +92,14 @@ public class MockHadoopTestingService {
 		return analytic_tech.startAnalyticJob(test_bucket, Arrays.asList(job.get()), job.get(), test_analytics_context);
 	}
 	
-	public void addFileToBucketStorage(final InputStream local_stream, final DataBucketBean bucket, String subservice_suffix) throws IOException {
+	/** Adds the contents of the InputStream to a file in the bucket's storage service
+	 * @param local_stream
+	 * @param bucket
+	 * @param subservice_suffix
+	 * @param date
+	 * @throws IOException
+	 */
+	public void addFileToBucketStorage(final InputStream local_stream, final DataBucketBean bucket, String subservice_suffix, Optional<Date> date) throws IOException {
 		final FileContext fileContext = _service_context.getStorageService().getUnderlyingPlatformDriver(FileContext.class,Optional.empty()).get();
 		final String bucketPath1 = _service_context.getStorageService().getBucketRootPath() + bucket.full_name();
 		
@@ -99,33 +108,65 @@ public class MockHadoopTestingService {
 			_cleared_dirs.add(bucketPath1);
 		}
 		
-		final String bucketReadyPath1 = bucketPath1 + subservice_suffix;
+		final String bucketReadyPath1 = bucketPath1 + subservice_suffix + date.map(d -> DateTimeFormatter.ofPattern("yyyy-MM-dd").format(d.toInstant())).orElse("");
 		DirUtils.createDirectory(fileContext,bucketReadyPath1);
 		DirUtils.createUTF8File(fileContext,bucketReadyPath1+"/data.json", new StringBuffer(IOUtils.toString(local_stream)));
 	}
 
+	/** Adds the contents of the InputStream to a file in the bucket's batch input path
+	 * @param local_stream
+	 * @param bucket
+	 * @throws IOException
+	 */
 	public void addFileToInputDirectory(final InputStream local_stream, final DataBucketBean bucket) throws IOException {
-		addFileToBucketStorage(local_stream, bucket, IStorageService.TO_IMPORT_DATA_SUFFIX);
+		addFileToBucketStorage(local_stream, bucket, IStorageService.TO_IMPORT_DATA_SUFFIX, Optional.empty());
 	}
 	
+	/** Adds the contents of the InputStream to a file in the bucket's transient batch job output
+	 * @param local_stream
+	 * @param bucket
+	 * @param job
+	 * @throws IOException
+	 */
 	public void addFileToTransientJobOutput(final InputStream local_stream, final DataBucketBean bucket, String job) throws IOException {
-		addFileToBucketStorage(local_stream, bucket, IStorageService.TRANSIENT_DATA_SUFFIX + job);
+		addFileToBucketStorage(local_stream, bucket, IStorageService.TRANSIENT_DATA_SUFFIX + job, Optional.empty());
 	}
 
-	public int numFilesInBucketStorage(final DataBucketBean bucket, final String subservice_suffix) {
+	/** Returns the number of files in the designated bucket's storage service
+	 * @param bucket
+	 * @param subservice_suffix
+	 * @param date
+	 * @return
+	 */
+	public int numFilesInBucketStorage(final DataBucketBean bucket, final String subservice_suffix, Optional<Date> date) {
 		final String bucketPath1 = _service_context.getStorageService().getBucketRootPath() + bucket.full_name();
-		final String bucketReadyPath1 = bucketPath1 + subservice_suffix;
+		final String bucketReadyPath1 = bucketPath1 + subservice_suffix + date.map(d -> DateTimeFormatter.ofPattern("yyyy-MM-dd").format(d.toInstant())).orElse("");
 		return Optional.ofNullable(new File(bucketReadyPath1).listFiles()).orElseGet(() -> new File[0]).length;
 	}
 
+	/** Returns the number of files in the designated bucket's batch input path
+	 * @param bucket
+	 * @return
+	 */
 	public int numFilesInInputDirectory(final DataBucketBean bucket) {
-		return numFilesInBucketStorage(bucket, IStorageService.TO_IMPORT_DATA_SUFFIX);
+		return numFilesInBucketStorage(bucket, IStorageService.TO_IMPORT_DATA_SUFFIX, Optional.empty());
 	}
 	
+	/** Returns the number of files in the designated bucket's transient batch job output
+	 * @param bucket
+	 * @param job
+	 * @return
+	 */
 	public int isFileInTransientJobOutput(final DataBucketBean bucket, final String job) {
-		return numFilesInBucketStorage(bucket, IStorageService.TRANSIENT_DATA_SUFFIX + job);
+		return numFilesInBucketStorage(bucket, IStorageService.TRANSIENT_DATA_SUFFIX + job, Optional.empty());
 	}
 	
+	/** Returns the number of records in the bucket's storage service
+	 * @param bucket
+	 * @return
+	 * @throws InterruptedException
+	 * @throws ExecutionException
+	 */
 	public long getNumRecordsInSearchIndex(final DataBucketBean bucket) throws InterruptedException, ExecutionException {
 		final IDataWriteService<JsonNode> es_index = 
 				_service_context.getSearchIndexService().get().getDataService().get().getWritableDataService(JsonNode.class, bucket, Optional.empty(), Optional.empty()).get();
