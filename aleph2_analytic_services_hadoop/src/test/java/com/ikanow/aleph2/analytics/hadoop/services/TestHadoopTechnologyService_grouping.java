7fef780634d2b8ef6ad51268350df9ba4f101e77
alex <alex@localhost>
2015-11-14 17:06:00 -0500

    ALEPH-74 Test and debug for grouping stage

--- aleph2_analytic_services_hadoop/src/test/java/com/ikanow/aleph2/analytics/hadoop/services/TestHadoopTechnologyService_grouping.java
+++ aleph2_analytic_services_hadoop/src/test/java/com/ikanow/aleph2/analytics/hadoop/services/TestHadoopTechnologyService_grouping.java
@@ -15,7 +15,7 @@
  *******************************************************************************/
 package com.ikanow.aleph2.analytics.hadoop.services;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
@@ -204,9 +204,127 @@ public class TestHadoopTechnologyService_grouping {
 		assertEquals(0L, test_service.numFilesInInputDirectory(test_bucket));
 		// (file added to output)
 		assertEquals(0L, test_service.numFilesInBucketStorage(test_bucket, IStorageService.STORED_DATA_SUFFIX_RAW, Either.left(Optional.empty())));
-		assertEquals(2L, test_service.numFilesInBucketStorage(test_bucket, IStorageService.STORED_DATA_SUFFIX_PROCESSED, Either.left(Optional.empty())));
+		assertTrue(test_service.numFilesInBucketStorage(test_bucket, IStorageService.STORED_DATA_SUFFIX_PROCESSED, Either.left(Optional.empty())) > 0);
+	}
+
+	@Test
+	public void test_enrichment_withGroupingThenMap() throws IOException, InterruptedException, ExecutionException {
+		if (isWindows()) return;
+		
+		// This is a duplicate of the testBeJobService but going via the full interface
+		
+		final AnalyticThreadJobInputBean test_analytic_input = 
+				BeanTemplateUtils.build(AnalyticThreadJobInputBean.class)
+					.with(AnalyticThreadJobInputBean::data_service, "batch")
+					.with(AnalyticThreadJobInputBean::resource_name_or_id, "")
+				.done().get();
+		
+		// Set passthrough topology
+		final AnalyticThreadJobBean test_analytic = BeanTemplateUtils.build(AnalyticThreadJobBean.class)
+				.with(AnalyticThreadJobBean::name, "simplejob")
+				.with(AnalyticThreadJobBean::module_name_or_id, "_module_test")
+				.with(AnalyticThreadJobBean::inputs, Arrays.asList(test_analytic_input))
+			.done().get();
+		
+		final DataBucketBean test_bucket = BeanTemplateUtils.build(DataBucketBean.class)
+												.with(DataBucketBean::full_name, "/test/simple/reduce/then/map")
+												.with(DataBucketBean::analytic_thread,
+														BeanTemplateUtils.build(AnalyticThreadBean.class)
+															.with(AnalyticThreadBean::jobs, Arrays.asList(test_analytic))
+														.done().get()
+														)
+												.with(DataBucketBean::batch_enrichment_configs, 
+														Arrays.asList(
+																BeanTemplateUtils.build(EnrichmentControlMetadataBean.class)																	
+																.done().get()
+																,
+																BeanTemplateUtils.build(EnrichmentControlMetadataBean.class)																	
+																.done().get()
+																,
+																BeanTemplateUtils.build(EnrichmentControlMetadataBean.class)
+																	.with(EnrichmentControlMetadataBean::grouping_fields, Arrays.asList("key"))
+																.done().get()
+																,
+																BeanTemplateUtils.build(EnrichmentControlMetadataBean.class)																	
+																.done().get()
+																,
+																BeanTemplateUtils.build(EnrichmentControlMetadataBean.class)																	
+																.done().get()
+																))
+												.with(DataBucketBean::data_schema,
+														BeanTemplateUtils.build(DataSchemaBean.class)
+															.with(DataSchemaBean::storage_schema,
+																BeanTemplateUtils.build(DataSchemaBean.StorageSchemaBean.class)
+																	.with(DataSchemaBean.StorageSchemaBean::processed,
+																			BeanTemplateUtils.build(DataSchemaBean.StorageSchemaBean.StorageSubSchemaBean.class)
+																				.with(DataSchemaBean.StorageSchemaBean.StorageSubSchemaBean::target_write_settings, 
+																						BeanTemplateUtils.build(DataSchemaBean.WriteSettings.class)
+																							.with(DataSchemaBean.WriteSettings::batch_flush_interval, 5)
+																						.done().get()
+																						)
+																			.done().get()
+																			)
+																.done().get()
+															)
+															.with(DataSchemaBean::search_index_schema,
+																BeanTemplateUtils.build(DataSchemaBean.SearchIndexSchemaBean.class)
+																.done().get()
+															)
+														.done().get()
+														)
+											.done().get();
+		
+		//Need to store this bucket so that multi-input validation works later
+		final IManagementDbService mgmt_db = _service_context.getService(IManagementDbService.class, Optional.empty()).get();
+		mgmt_db.getDataBucketStore().storeObject(test_bucket, true).join();
+		
+		final MockHadoopTestingService test_service = new MockHadoopTestingService(_service_context);
+		
+		test_service.clearAllDataForBucket(test_bucket);
+		assertEquals(0L, test_service.getNumRecordsInSearchIndex(test_bucket));
+		assertEquals(0L, test_service.numFilesInInputDirectory(test_bucket));
+		assertEquals(0L, test_service.numFilesInBucketStorage(test_bucket, IStorageService.STORED_DATA_SUFFIX_RAW, Either.left(Optional.empty())));
+		assertEquals(0L, test_service.numFilesInBucketStorage(test_bucket, IStorageService.STORED_DATA_SUFFIX_PROCESSED, Either.left(Optional.empty())));
+		
+		final InputStream test_file1 = new ByteArrayInputStream("{\"key\":\"group1\"}".getBytes(StandardCharsets.UTF_8));
+		final InputStream test_file2 = new ByteArrayInputStream("{\"key\":\"group1\"}".getBytes(StandardCharsets.UTF_8));
+		final InputStream test_file3 = new ByteArrayInputStream("{\"key\":\"group2\"}".getBytes(StandardCharsets.UTF_8));
+		final InputStream test_file4 = new ByteArrayInputStream("{\"key\":\"group2\"}".getBytes(StandardCharsets.UTF_8));
+		test_service.addFileToInputDirectory(test_file1, test_bucket);
+		test_service.addFileToInputDirectory(test_file2, test_bucket);
+		test_service.addFileToInputDirectory(test_file3, test_bucket);
+		test_service.addFileToInputDirectory(test_file4, test_bucket);
+		
+		test_service.testAnalyticModule(test_bucket, Optional.empty());
+
+		// Wait for job to finish
+		for (int ii = 0; ii < 120; ++ii) {
+			Thread.sleep(1000L);
+			if (test_service.isJobComplete(test_bucket)) {
+				break;
+			}
+		}
+		for (int ii = 0; ii < 40; ++ii) {
+			Thread.sleep(500L);
+			if ((test_service.getNumRecordsInSearchIndex(test_bucket) >= 4)
+					&&
+					(test_service.numFilesInBucketStorage(test_bucket, IStorageService.STORED_DATA_SUFFIX_PROCESSED, Either.left(Optional.empty())) > 0)	
+				)
+			{
+				break;
+			}
+		}
+		
+		// Check results:
+		
+		// (got an ES record)
+		assertEquals(4L, test_service.getNumRecordsInSearchIndex(test_bucket));
+		// (file removed from input)
+		assertEquals(0L, test_service.numFilesInInputDirectory(test_bucket));
+		// (file added to output)
+		assertEquals(0L, test_service.numFilesInBucketStorage(test_bucket, IStorageService.STORED_DATA_SUFFIX_RAW, Either.left(Optional.empty())));
+		assertTrue(test_service.numFilesInBucketStorage(test_bucket, IStorageService.STORED_DATA_SUFFIX_PROCESSED, Either.left(Optional.empty())) > 0);
 	}
 	
-	//TODO: now have a map stage immediately after the reduce stage
-	//TODO: now have an automated grouping stage
+	//TODO: now have an automated grouping stage (use the count test that is kicking around)
 }
