cfa77fe77271d60f388e96af9fc9d75184e0c91e
jfreydank <jfreydank@ikanow.com>
2015-12-03 12:07:21 -0600

    Added functions to IkanowV1WebSecurityModule.

--- aleph2_security_service_ikanow_v1/src/com/ikanow/aleph2/security/module/IkanowV1WebSecurityModule.java
+++ aleph2_security_service_ikanow_v1/src/com/ikanow/aleph2/security/module/IkanowV1WebSecurityModule.java
@@ -1,28 +1,40 @@
 package com.ikanow.aleph2.security.module;
 
-
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.session.mgt.SessionManager;
+import com.google.inject.binder.AnnotatedBindingBuilder;
 
 public class IkanowV1WebSecurityModule extends IkanowV1SecurityModule {
 
 	@Override
+	/**
+	 * Partial functionality from ShiroModule. We do not want the whole guice functionality because the WebSecurityManager is created using the IniFactory.
+	 */
 	public void configure() {
-		configureShiro();
-		
-	}
 
+        configureShiro();
+        bind(realmCollectionKey())
+                .to(realmSetKey());
+        expose(realmCollectionKey());
 
-	@Override
-	protected void bindRealms() {
-		// TODO Auto-generated method stub
-		super.bindRealms();
-	}
+    }
 
 	@Override
-	protected void bindRoleProviders() {
-		// TODO Auto-generated method stub
-		super.bindRoleProviders();
-	}
+    protected void bindSecurityManager(AnnotatedBindingBuilder<? super SecurityManager> bind) {
+		// empty on purpose because we are using the WebSecurityManager created by the IniWebEnvironment
+    }
 
-	
+    /**
+     * Binds the session manager.  Override this method in order to provide your own session manager binding.
+     * <p/>
+     * By default, a {@link org.apache.shiro.session.mgt.DefaultSessionManager} is bound as an eager singleton.
+     *
+     * @param bind
+     */
+	@Override
+    protected void bindSessionManager(AnnotatedBindingBuilder<SessionManager> bind) {
+		// empty on purpose because we are using the WebSecurityManager created by the IniWebEnvironment
+    }
+
 
 }
