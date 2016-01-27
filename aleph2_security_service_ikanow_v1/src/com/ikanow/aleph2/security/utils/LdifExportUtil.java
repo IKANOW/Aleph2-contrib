af1d1319254cc9d249934e254ce74896bbf91311
jfreydank <jfreydank@ikanow.com>
2016-01-27 17:48:56 -0600

    Removed import.

--- aleph2_security_service_ikanow_v1/src/com/ikanow/aleph2/security/utils/LdifExportUtil.java
+++ aleph2_security_service_ikanow_v1/src/com/ikanow/aleph2/security/utils/LdifExportUtil.java
@@ -28,7 +28,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
-import java.util.concurrent.ExecutionException;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
